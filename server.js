776f6d180db245e10650524bd91996385c692d9b
superbabii <nzrbabii@gmail.com>
2024-10-31 20:14:48 +0200

    finish

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Add CORS
 
 const app = express();
+app.use(cors()); // Enable CORS
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
